744d1d971b3a262d43772a0b362ec7c249037b8b
Mostafatahadev1 <mostafatahadev1@gmail.com>
2026-06-25 06:22:42 +0300

    Add project files.

---
+++ GymAppLecture/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using GymAppLecture.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymAppLecture.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
