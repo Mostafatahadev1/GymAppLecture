5e2fa4e35ea8d9a2f1cf9e3e2ed6be02f4356635
Mostafatahadev1 <mostafatahadev1@gmail.com>
2026-06-25 07:55:23 +0300

    Index Plans

---
+++ GymAppLecture/Controllers/PlansContoroller.cs
@@ -0,0 +1,31 @@
+﻿using GymAppLecture.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAppLecture.Controllers
+{
+    public class PlansCentroller : Controller
+    {
+
+        private readonly GymDbContext dbContext;
+
+        public PlansCentroller()
+        {
+            dbContext = new GymDbContext();
+        }
+
+        // Index Action
+        // GET BaseUrl/PlansContoroller/Index => List of Plans
+
+
+        public async Task <IActionResult> Index()
+        {
+            var plans = dbContext.Plans.ToListAsync();
+            return View(plans);
+        }
+
+        //Details Action
+
+        //Get BaseUrl/PlansContoroller/Details/{id} => Details of a Plan
+    }
+}
